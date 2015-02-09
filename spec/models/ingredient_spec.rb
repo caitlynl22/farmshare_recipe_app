64f4ab848dce4017327d8a3f644e4568744d9bda
Caitlyn Landry <caitlyn.landry@gmail.com>
2015-02-08 20:32:26 -0500

    Pass RSpec tests for join table

--- spec/models/ingredient_spec.rb
+++ spec/models/ingredient_spec.rb
@@ -18,8 +18,17 @@ RSpec.describe Ingredient do
 
     ingredient.recipes << recipe
 
-    RecipeIngredient.first.ingredient.should == ingredient
-    RecipeIngredient.first.recipe.should == recipe
+    expect(RecipeIngredient.first.ingredient).to eq(ingredient)
+    expect(RecipeIngredient.first.recipe).to eq(recipe)
     end
+
+    it "has recipes" do
+    ingredient = Ingredient.create!(name: 'Carrots')
+    recipe = Recipe.create!(title: 'Garden Fries', ingredient_list: "1 lb of root veggies, 1 tbsp olive oil, 1/8 tsp ground red pepper, 1/8 tsp garlic powder, 1/2 tsp Worcestershire sauce", instructions: "Preheat oven to 450. In a large bowl combine the sliced vegetables, olive oil, red pepper, garlic powder and Worcestershire and toss to coat well. Lay the vegetables out on a baking sheet in a single layer. Bake for 30 minutes, turning once. Serve the fries hot with your favorite sauce or ketchup!")
+
+    ingredient.recipes << recipe
+
+    expect(ingredient.recipes).to eq([recipe])
+  end
   end
 end
