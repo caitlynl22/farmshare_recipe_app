27b664fde70dab7e89b6e814f9a261214bf18119
Caitlyn Landry <caitlyn.landry@gmail.com>
2015-02-09 02:07:47 -0500

    Add Recipe validations and tests

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,5 @@
 class Recipe < ActiveRecord::Base
+  validates :title, :ingredient_list, :instructions, presence: true
 
   has_many :recipe_ingredients
   has_many :ingredients, through: :recipe_ingredients
--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -5,5 +5,21 @@ RSpec.describe Recipe do
     it 'creates a new recipe' do
       expect(Recipe.create()).to be_a Recipe
     end
+
+    it 'is invalid without a title' do
+      expect(Recipe.create(title: '', ingredient_list: "1 lb of root veggies, 1 tbsp olive oil, 1/8 tsp ground red pepper, 1/8 tsp garlic powder, 1/2 tsp Worcestershire sauce", instructions: "Preheat oven to 450. In a large bowl combine the sliced vegetables, olive oil, red pepper, garlic powder and Worcestershire and toss to coat well. Lay the vegetables out on a baking sheet in a single layer. Bake for 30 minutes, turning once. Serve the fries hot with your favorite sauce or ketchup!")).not_to be_valid
+    end
+
+    it 'is invalid without an ingredient list' do
+      expect(Recipe.create(title: 'Garden Fries', ingredient_list: '', instructions: "Preheat oven to 450. In a large bowl combine the sliced vegetables, olive oil, red pepper, garlic powder and Worcestershire and toss to coat well. Lay the vegetables out on a baking sheet in a single layer. Bake for 30 minutes, turning once. Serve the fries hot with your favorite sauce or ketchup!")).not_to be_valid
+    end
+
+    it 'is invalid without instructions' do
+      expect(Recipe.create(title: 'Garden Fries', ingredient_list: "1 lb of root veggies, 1 tbsp olive oil, 1/8 tsp ground red pepper, 1/8 tsp garlic powder, 1/2 tsp Worcestershire sauce", instructions: '')).not_to be_valid
+    end
+
+    it 'is valid with a title, ingredient list, and instructions' do
+      expect(Recipe.create(title: 'Garden Fries', ingredient_list: "1 lb of root veggies, 1 tbsp olive oil, 1/8 tsp ground red pepper, 1/8 tsp garlic powder, 1/2 tsp Worcestershire sauce", instructions: "Preheat oven to 450. In a large bowl combine the sliced vegetables, olive oil, red pepper, garlic powder and Worcestershire and toss to coat well. Lay the vegetables out on a baking sheet in a single layer. Bake for 30 minutes, turning once. Serve the fries hot with your favorite sauce or ketchup!")).to be_valid
+    end
   end
 end
