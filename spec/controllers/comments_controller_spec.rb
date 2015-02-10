9bbd5c71794eb6df2a6050486bf6c4e520b72301
Caitlyn Landry <caitlyn.landry@gmail.com>
2015-02-10 17:46:01 -0500

    Pass all comment controller tests with valid attributes

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -19,9 +19,18 @@ RSpec.describe CommentsController do
       end
 
       it 'assigns @comment' do
-        post :create, comment: valid_attributes
+        ingredient = Ingredient.create!(name: 'Carrots')
+        recipe = ingredient.recipes.create!(title: 'Garden Fries', ingredient_list: "1 lb of root veggies, 1 tbsp olive oil, 1/8 tsp ground red pepper, 1/8 tsp garlic powder, 1/2 tsp Worcestershire sauce", instructions: "Preheat oven to 450. In a large bowl combine the sliced vegetables, olive oil, red pepper, garlic powder and Worcestershire and toss to coat well. Lay the vegetables out on a baking sheet in a single layer. Bake for 30 minutes, turning once. Serve the fries hot with your favorite sauce or ketchup!")
+        post :create, comment: valid_attributes, recipe_id: recipe
         expect(assigns(:comment)).to be_a Comment
         expect(assigns(:comment)).to be_persisted
+      end
+
+      it 'redirects to the recipe show page on which the comment was created' do
+        ingredient = Ingredient.create!(name: 'Carrots')
+        recipe = ingredient.recipes.create!(title: 'Garden Fries', ingredient_list: "1 lb of root veggies, 1 tbsp olive oil, 1/8 tsp ground red pepper, 1/8 tsp garlic powder, 1/2 tsp Worcestershire sauce", instructions: "Preheat oven to 450. In a large bowl combine the sliced vegetables, olive oil, red pepper, garlic powder and Worcestershire and toss to coat well. Lay the vegetables out on a baking sheet in a single layer. Bake for 30 minutes, turning once. Serve the fries hot with your favorite sauce or ketchup!")
+        post :create, comment: valid_attributes, recipe_id: recipe
+        expect(response).to redirect_to(recipe)
       end
     end
   end
