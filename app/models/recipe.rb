0749c6a44bfee380f57064a8f3b8da5496bb0f45
Caitlyn Landry <caitlyn.landry@gmail.com>
2015-02-11 15:33:44 -0500

    More styling changes, add images, customized nav bar.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,4 +4,10 @@ class Recipe < ActiveRecord::Base
   has_many :recipe_ingredients
   has_many :ingredients, through: :recipe_ingredients
   has_many :comments, dependent: :destroy
+
+  def vegetarian
+    if :vegetarian == true
+      render "images/vegetarian.png"
+    end
+  end
 end
